574cd5505a6bb8aa3771ca94a0e0c6ac46769062
victorlicht <mihoubiabderrahmane09@gmail.com>
2023-11-24 23:56:38 +0100

    Build Employee Components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import { useState } from 'react';
 import Employee from './components/Employee';
+import man from './assets/man.jpeg'
+import dog from './assets/dog.webp'
+import cat from './assets/photo_2023-11-24_23-12-13.jpg'
+import { useState } from 'react';
 function App() {
-
-  const [role, setRole] = useState();
-  const showEmployees = true;
+  const [employees, setEmployees] = useState(
+    [
+      {
+        name: 'Zino',
+        role: 'Engineer',
+        img : man
+      },
+      {
+        name: 'El-Boun',
+        role: 'Tiger-Lion',
+        img : cat
+      },
+      {
+        name: 'Bolbol',
+        role: 'Dog Mechanic',
+        img : dog
+      }
+    ]
+  );
   return (
-    <div className="App" class="text-center m-4">
-      <header className="App-header">
-        {showEmployees ? (
-          <>
-            <input class="border-2 border-solid border-black bg" type="text" onChange={(e) => {
-              console.log(e.target.value);
-              setRole(e.target.value);
-            }}
-            />
-            <Employee name="Zino" role="Engineer"/>
-            <Employee name="Chihab" role={role}/>
-        
-          </>
-        ) : (
-          <p>You cannot see the employees</p>
-        )}
-      </header>
+    <div className="App flex flex-wrap justify-center align-middle text-center m-4">
+      {employees.map((employee) => {
+        return (
+          <Employee name={employee.name} role={employee.role} img={employee.img} />
+        );
+      })}
     </div>
   );
 }
